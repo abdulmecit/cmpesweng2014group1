9829577d17b9addb38dcd64d04b8111bddf51541
burcukilicaslan91@gmail.com <burcukilicaslan91@gmail.com>
2014-10-29 18:10:52 +0200

    Signup implementation. SignupServlet and LoginManager class are created, addUser method is added. (It will be updated)

---
+++ project451/src/SignupServlet.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import project451.LoginManager;
+
+/**
+ * Servlet implementation class SignupServlet
+ */
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 String email = request.getParameter("email");
+		 String password = request.getParameter("password");
+		 response.setContentType("text/plain");
+	     response.setCharacterEncoding("UTF-8");
+		 LoginManager LogMan=new LoginManager();
+		 String res=LogMan.addUser(email, password);
+		 response.getWriter().write(res);	 
+	}
+
+}
---
+++ project451/src/project451/LoginManager.java
@@ -0,0 +1,54 @@
+package project451;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LoginManager {
+
+	public String addUser(String email, String password){
+		Connection con;
+        Statement stmt;
+        try{
+        	Class.forName("com.mysql.jdbc.Driver");
+        	con = DriverManager.getConnection("jdbc:mysql://titan.cmpe.boun.edu.tr:3306/database1", "project1", "2TJZD32R");
+        	stmt = con.createStatement();
+        }
+        catch(Exception e){
+        	return "Database Opening Connection Error: " + e.toString();	
+        }
+        ResultSet rs;
+        int count;
+        try {
+			rs = stmt.executeQuery("SELECT * FROM database1.Users WHERE email='" + email + "'");
+			rs.last();
+			count = rs.getRow();
+			rs.beforeFirst();
+        } catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			return "Database Error: " + e1.toString();
+		}       
+        if(count!=0){
+        	return "This email is already registered!";
+        }
+        String username="deneme";
+        String query = "INSERT INTO database1.Users (email, password, username) VALUES ('" + email + "', '" + password + "', '" + username + "')";
+		try{
+		stmt.executeUpdate(query);
+		}
+		catch(Exception e){
+			return "Database Insertion Error: " + e.toString();
+		}  
+		//Close all
+		try {
+			con.close();
+		} 
+		catch (Exception e) {
+			return "Database Closing Connection Error: " + e.toString();
+		}  
+		return "Success";
+	}
+	
+}
