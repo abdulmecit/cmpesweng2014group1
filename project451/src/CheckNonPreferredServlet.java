160624b4221a2363095f9380c67d8f7fdbf7f29a
burcukilicaslan91@gmail.com <burcukilicaslan91@gmail.com>
2014-11-05 17:21:11 +0200

    Profil information (Diseases, Food Intolerance and Allergies)

---
+++ project451/src/CheckNonPreferredServlet.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import project451.FoodSelectionManager;
+
+/**
+ * Servlet implementation class CheckNonPreferredServlet
+ */
+@WebServlet("/CheckNonPreferredServlet")
+public class CheckNonPreferredServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckNonPreferredServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/plain");
+	    response.setCharacterEncoding("UTF-8");
+	    HttpSession session = request.getSession(false);
+	    int user_id=Integer.valueOf(session.getAttribute("user_id").toString());
+	    FoodSelectionManager FoodSel=new FoodSelectionManager();
+	    String res=FoodSel.getNonPreferred(user_id);
+	}
+
+}
--- project451/src/project451/FoodSelectionManager.java
+++ project451/src/project451/FoodSelectionManager.java
@@ -153,4 +153,204 @@ public class FoodSelectionManager {
 		}  
 		return "Success!";
 	}
+	
+	public String getNonPreferred(int user_id){
+		Connection con;
+        try{
+        	Class.forName("com.mysql.jdbc.Driver");
+        	con = DriverManager.getConnection("jdbc:mysql://titan.cmpe.boun.edu.tr:3306/database1", "project1", "2TJZD32R");
+        }
+        catch(Exception e){
+        	return "Database Opening Connection Error: " + e.toString();	
+        }      
+        PreparedStatement ps;
+		try{
+	    	ps = con.prepareStatement("SELECT ing_id FROM database1.Unprefer WHERE user_id=?");
+	        ps.setInt(1, user_id);
+		}
+		catch(Exception e){
+			return "Query Building Error: " + e.toString();	
+		}
+		ResultSet rs;
+        int count;
+		try{
+        	rs = ps.executeQuery();
+			rs.last();
+			count = rs.getRow();
+		}
+		catch(Exception e){
+			return "Query Running Error: " + e.toString();	
+		}
+
+        if(count == 0){ //no preference
+        	return "";
+        }
+        String nonPreferred="";
+		try{
+			rs.beforeFirst();
+			while(rs.next()){
+				nonPreferred +=rs.getString(1)+"%";
+			}
+		}
+		catch(Exception e){
+    		return "Result Processing Error: " + e.toString();
+		}
+		return nonPreferred;     
+        
+	}
+	public String getHealthCondition(int user_id){
+		Connection con;
+        try{
+        	Class.forName("com.mysql.jdbc.Driver");
+        	con = DriverManager.getConnection("jdbc:mysql://titan.cmpe.boun.edu.tr:3306/database1", "project1", "2TJZD32R");
+        }
+        catch(Exception e){
+        	return "Database Opening Connection Error: " + e.toString();	
+        }      
+        PreparedStatement ps;
+		try{
+	    	ps = con.prepareStatement("SELECT fs_id FROM database1.HasSelection WHERE user_id=?");
+	        ps.setInt(1, user_id);
+		}
+		catch(Exception e){
+			return "Query Building Error: " + e.toString();	
+		}
+		ResultSet rs;
+        int count;
+		try{
+        	rs = ps.executeQuery();
+			rs.last();
+			count = rs.getRow();
+		}
+		catch(Exception e){
+			return "Query Running Error: " + e.toString();	
+		}
+
+        if(count == 0){ //no preference
+        	return "";
+        }
+        String conditions="";
+        int fs_id=0;
+		try{
+			rs.beforeFirst();
+			while(rs.next()){
+				fs_id=rs.getInt(1);
+				PreparedStatement ps2;
+				try{
+			    	ps2 = con.prepareStatement("SELECT fs_name FROM database1.FoodSelection WHERE fs_id=? AND type=?");
+			        ps2.setInt(1,fs_id );
+			        ps2.setString(1, "health_condition");
+				}
+				catch(Exception e){
+					return "Query Building Error: " + e.toString();	
+				}
+				ResultSet rs2;
+		        int count2;
+				try{
+		        	rs2 = ps2.executeQuery();
+					rs2.last();
+					count2 = rs2.getRow();
+				}
+				catch(Exception e){
+					return "Query Running Error: " + e.toString();	
+				}
+
+		        if(count2 == 0){ //never reach??
+		        	return "";
+		        }
+				try{
+					rs2.beforeFirst();
+					while(rs2.next()){
+						conditions +=rs2.getString(1)+"%";
+					}
+				}
+				catch(Exception e){
+		    		return "Result Processing Error: " + e.toString();
+				}	
+			}
+		}
+		catch(Exception e){
+    		return "Result Processing Error: " + e.toString();
+		}
+		return conditions;     
+        
+	}
+	
+	public String getFoodIntolerance(int user_id){
+		Connection con;
+        try{
+        	Class.forName("com.mysql.jdbc.Driver");
+        	con = DriverManager.getConnection("jdbc:mysql://titan.cmpe.boun.edu.tr:3306/database1", "project1", "2TJZD32R");
+        }
+        catch(Exception e){
+        	return "Database Opening Connection Error: " + e.toString();	
+        }      
+        PreparedStatement ps;
+		try{
+	    	ps = con.prepareStatement("SELECT fs_id FROM database1.HasSelection WHERE user_id=?");
+	        ps.setInt(1, user_id);
+		}
+		catch(Exception e){
+			return "Query Building Error: " + e.toString();	
+		}
+		ResultSet rs;
+        int count;
+		try{
+        	rs = ps.executeQuery();
+			rs.last();
+			count = rs.getRow();
+		}
+		catch(Exception e){
+			return "Query Running Error: " + e.toString();	
+		}
+
+        if(count == 0){ //no preference
+        	return "";
+        }
+        String intolerances="";
+        int fs_id=0;
+		try{
+			rs.beforeFirst();
+			while(rs.next()){
+				fs_id=rs.getInt(1);
+				PreparedStatement ps2;
+				try{
+			    	ps2 = con.prepareStatement("SELECT fs_name FROM database1.FoodSelection WHERE fs_id=? AND type=?");
+			        ps2.setInt(1,fs_id );
+			        ps2.setString(1, "food_intolerance");
+				}
+				catch(Exception e){
+					return "Query Building Error: " + e.toString();	
+				}
+				ResultSet rs2;
+		        int count2;
+				try{
+		        	rs2 = ps2.executeQuery();
+					rs2.last();
+					count2 = rs2.getRow();
+				}
+				catch(Exception e){
+					return "Query Running Error: " + e.toString();	
+				}
+
+		        if(count2 == 0){ //never reach??
+		        	return "";
+		        }
+				try{
+					rs2.beforeFirst();
+					while(rs2.next()){
+						intolerances +=rs2.getString(1)+"%";
+					}
+				}
+				catch(Exception e){
+		    		return "Result Processing Error: " + e.toString();
+				}	
+			}
+		}
+		catch(Exception e){
+    		return "Result Processing Error: " + e.toString();
+		}
+		return intolerances;     
+        
+	}
 }
